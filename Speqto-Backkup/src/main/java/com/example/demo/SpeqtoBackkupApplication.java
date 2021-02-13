90d1977ddff6545c88ed623bd34dd8d141d80aa6
vivek-speqto <vivek.singh@speqto.com>
2021-02-13 22:43:22 +0530

    First Commit

---
+++ Speqto-Backkup/src/main/java/com/example/demo/SpeqtoBackkupApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeqtoBackkupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeqtoBackkupApplication.class, args);
+	}
+
+}
